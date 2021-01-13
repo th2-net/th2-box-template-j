26f5efc596155bcddcce3632557bd5e4d8ad78f8
Oleg Smirnov <oleg.smirnov@exactprosystems.com>
2021-01-13 13:18:33 +0300

    Correct template project
    * add shutdown hook and termination awaiting
    * correct base image version
    * add .gitlab-ci configuration
    * add copyright settings

--- src/main/kotlin/com/exactpro/th2/template/BoxMain.kt
+++ src/main/kotlin/com/exactpro/th2/template/BoxMain.kt
@@ -24,21 +24,42 @@ import com.exactpro.th2.common.metrics.liveness
 import com.exactpro.th2.common.metrics.readiness
 import com.exactpro.th2.common.schema.factory.CommonFactory
 import mu.KotlinLogging
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedDeque
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.thread
+import kotlin.system.exitProcess
 
 private val LOGGER = KotlinLogging.logger { }
 
 fun main(args: Array<String>) {
     LOGGER.info { "Starting the box" }
     // Here is an entry point to the th2-box.
-    // You need to initialize the CommonFactory
 
-    // You can use custom paths to each config that is required for the CommonFactory
-    // If args are empty the default path will be chosen.
-    val factory = CommonFactory.createFromArguments(*args)
+    // Configure shutdown hook for closing all resources
+    // and the lock condition to await termination.
+    //
+    // If you use the logic that doesn't require additional threads
+    // and you can run everything on main thread
+    // you can omit the part with locks (but please keep the resources queue)
+    val resources: Deque<AutoCloseable> = ConcurrentLinkedDeque()
+    val lock = ReentrantLock()
+    val condition: Condition = lock.newCondition()
+    configureShutdownHook(resources, lock, condition)
 
-    // The BOX is alive
-    liveness = true
     try {
+        // You need to initialize the CommonFactory
+
+        // You can use custom paths to each config that is required for the CommonFactory
+        // If args are empty the default path will be chosen.
+        val factory = CommonFactory.createFromArguments(*args)
+        // do not forget to add resource to the resources queue
+        resources += factory
+
+        // The BOX is alive
+        liveness = true
+
         val eventRouter = factory.eventBatchRouter
         eventRouter.send(
             EventBatch.newBuilder()
@@ -49,16 +70,52 @@ fun main(args: Array<String>) {
                 )
                 .build()
         )
+
+        // Do additional initialization required to your logic
+
         // The BOX is ready to work
         readiness = true
 
-        // do some work
+        awaitShutdown(lock, condition)
+    } catch (ex: Exception) {
+        LOGGER.error(ex) { "Cannot start the box" }
+        exitProcess(1)
+    }
+}
 
-    } finally {
+private fun configureShutdownHook(resources: Deque<AutoCloseable>, lock: ReentrantLock, condition: Condition) {
+    Runtime.getRuntime().addShutdownHook(thread(
+        start = false,
+        name = "Shutdown hook"
+    ) {
+        LOGGER.info { "Shutdown start" }
         readiness = false
+        try {
+            lock.lock()
+            condition.signalAll()
+        } finally {
+            lock.unlock()
+        }
+        resources.descendingIterator().forEachRemaining { resource ->
+            try {
+                resource.close()
+            } catch (e: Exception) {
+                LOGGER.error(e) { "Cannot close resource ${resource::class}" }
+            }
+        }
         liveness = false
-        // need to close resources
-        // factory closes all related resources (routers, gRPC, Cradle, RabbitMQ) as well
-        factory.close()
+        LOGGER.info { "Shutdown end" }
+    })
+}
+
+@Throws(InterruptedException::class)
+private fun awaitShutdown(lock: ReentrantLock, condition: Condition) {
+    try {
+        lock.lock()
+        LOGGER.info { "Wait shutdown" }
+        condition.await()
+        LOGGER.info { "App shutdown" }
+    } finally {
+        lock.unlock()
     }
 }
